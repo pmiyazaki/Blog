3ec0e963bff8c0e074e749280053f7199d3f6478
Patricia <pmiyazakii@gmail.com>
2021-10-08 18:40:40 -0300

    Third commit

--- src/main/java/com/DigitalHouse/blog/controller/PostController.java
+++ src/main/java/com/DigitalHouse/blog/controller/PostController.java
@@ -1,12 +1,19 @@
 package com.DigitalHouse.blog.controller;
 
+import com.DigitalHouse.blog.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/postagem")
+@CrossOrigin("*")
 public class PostController {
+
+    @Autowired
+    private PostagemRepository repository;
 
     @GetMapping
     public String getPosts() {
---
+++ src/main/java/com/DigitalHouse/blog/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.DigitalHouse.blog.repository;
+
+import com.DigitalHouse.blog.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    //consulta pelo titulo
+    public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+}
+
